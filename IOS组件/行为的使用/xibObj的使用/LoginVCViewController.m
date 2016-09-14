dc5385c87639d3c50daccafffdfa12a4a114d515
81516741 <you@example.com>
2016-09-14 15:18:33 +0800

    遗留一个文件未提交

---
+++ IOS组件/行为的使用/xibObj的使用/LoginVCViewController.m
@@ -0,0 +1,17 @@
+//
+//  LoginVCViewController.m
+//  xibObj的使用
+//
+//  Created by ld on 16/9/14.
+//  Copyright © 2016年 ld. All rights reserved.
+//
+
+#import "LoginVCViewController.h"
+
+@implementation LoginVCViewController
+
+- (IBAction)dismissSelf:(id)sender {
+    [self dismissViewControllerAnimated:true completion:nil];
+}
+
+@end
